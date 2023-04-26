163f23ad7cd4276333dc80fd83dba219d931e253
Martin Nester <martinn7352@gmail.com>
2023-04-26 17:02:42 -0400

    test should only compare number of colors

--- exact_solution/run_test_cases.sh
+++ exact_solution/run_test_cases.sh
@@ -2,20 +2,32 @@ cd tests
 
 RED="\033[0;31m"
 GREEN="\033[0;32m"
+BOLD="\033[1m"
 NC="\033[0m" # No Color
+BLUE="\033[0;34m"
+UL="\e[4;30m"
 
-for case in case*
+
+echo -e "${UL}test\tresult\truntime${NC}"
+for test in *
 do
-    cd $case
+    cd $test
+
+    start=`python3 -c 'import time; print(time.time())'`
     python3 ../../solution.py < input > output
-    diff expected output > diff
-    if [ -s diff ]
+    end=`python3 -c 'import time; print(time.time())'`
+
+    runtime=$( echo "$end - $start" | bc -l )
+
+    x=$(head -n 1 expected)
+    y=$(head -n 1 output)
+
+    if [ "$x" = "$y" ]
     then
-        echo -e "$case: ${RED}failed${NC}"
+        echo -e "${test}\t${GREEN}passed\t${BLUE}${runtime}s${NC}"
     else
-        echo -e "$case: ${GREEN}passed${NC}"
+        echo -e "${test}\t${RED}failed\t${BLUE}${runtime}s${NC}"
     fi
-    rm diff
     cd ../
 done
 
